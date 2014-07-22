03d85e3cabdb3b30390a2f289ab03a17c731c0d5
seedante <seedante@gmail.com>
2014-07-22 09:59:26 +0800

    Initial Commit

---
+++ FaceAlbum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FaceAlbum
+//
+//  Created by seedante on 14-7-22.
+//  Copyright (c) 2014年 seedante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
---
+++ FaceAlbumTests/FaceAlbumTests.m
@@ -0,0 +1,34 @@
+//
+//  FaceAlbumTests.m
+//  FaceAlbumTests
+//
+//  Created by seedante on 14-7-22.
+//  Copyright (c) 2014年 seedante. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FaceAlbumTests : XCTestCase
+
+@end
+
+@implementation FaceAlbumTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
