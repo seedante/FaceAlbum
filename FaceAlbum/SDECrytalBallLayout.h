25cc82c4c1ceaf3b5c1cd2a36a9dae1223708631
seedante <seedante@gmail.com>
2014-12-31 01:11:23 +0800

    小合并前备份

---
+++ FaceAlbum/SDECrytalBallLayout.h
@@ -0,0 +1,13 @@
+//
+//  SDECrytalBallLayout.h
+//  FaceAlbum
+//
+//  Created by seedante on 12/31/14.
+//  Copyright (c) 2014 seedante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDECrytalBallLayout : UICollectionViewFlowLayout
+
+@end
---
+++ FaceAlbum/SDECrytalBallLayout.m
@@ -0,0 +1,13 @@
+//
+//  SDECrytalBallLayout.m
+//  FaceAlbum
+//
+//  Created by seedante on 12/31/14.
+//  Copyright (c) 2014 seedante. All rights reserved.
+//
+
+#import "SDECrytalBallLayout.h"
+
+@implementation SDECrytalBallLayout
+
+@end
